fe889c4e59928a03395cb811c05456b624952d90
Yerox-dev <77878263+Yerox-dev@users.noreply.github.com>
2021-02-09 19:22:37 +0100

    Added cmd_ban

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -16,6 +16,21 @@ class Moderation(commands.Cog):
         await member.send(embed=embed)
         await member.kick(reason=reason)
 
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        embed = discord.Embed(color=discord.Color.orange(), title=f":boom: Banned {member}",
+                              description=f"{member} was banned from the Server :no_entry:\nReason: {reason}")
+        if reason is None:
+            await ctx.send(embed=discord.Embed(color=discord.Color.red(), title="Missing Argument", description="You need to specify a reason!"))
+            return
+        if member == ctx.message.author:
+            await ctx.send(embed=discord.Embed(color=discord.Color.red(), title="Error", description="You can't ban yourself"))
+            return
+        await ctx.send(embed=embed)
+        await member.send(embed=embed)
+        await member.ban(reason=reason)
+
 
 def setup(client):
     client.add_cog(Moderation(client))
